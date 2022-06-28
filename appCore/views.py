0b45f837548ca3d2acaab4c46233052ed97e0444
MaercioMamedes <maerciomamedes@hotmail.com>
2022-06-28 07:50:43 -0300

    search implementation

--- appCore/views.py
+++ appCore/views.py
@@ -5,7 +5,7 @@ from .models import Recipe
 # views of app.
 
 def index(request):
-    recipe_list = Recipe.objects.order_by('-date_recipe').filter(published=True)
+    recipe_list = Recipe.objects.order_by('date_recipe').filter(published=True)
     recipe_list_show = {
         'recipe_list': recipe_list
     }
@@ -20,3 +20,19 @@ def recipe(request, recipe_id):
     }
 
     return render(request, 'receita.html', recipe_show)
+
+
+def search(request):
+    recipe_list = Recipe.objects.order_by('date_recipe').filter(published=True)
+
+    if 'search' in request.GET:
+        name_to_search = request.GET['search']
+
+        if search:
+            recipe_list = recipe_list.filter(name_recipe__icontains=name_to_search)
+
+    recipe_list_show = {
+        'recipe_list': recipe_list
+    }
+
+    return render(request, 'index.html', recipe_list_show)
